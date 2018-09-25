896a4ffd218cf756ca77b5ab7697b3455d9ab970
jamhir789 <erick.medina@itspanuco.edu.mx>
2018-09-25 16:05:18 -0500

    first commit

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public InterfacePais Repositorio{ get;}
+        public HomeController(InterfacePais repositorio)
+        {
+            Repositorio = repositorio;
+
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var paises = Repositorio.Obtener();
+            return View(paises);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Services/PaisMemoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services
+{
+    public class PaisMemoria:InterfacePais
+    {
+        public List<string> Obtener()
+        {
+            List<string> paises = new List<string>() { "mexico", "peru", "venesuela", "japon", "china" };
+
+            return paises;
+        }
+    }
+}
